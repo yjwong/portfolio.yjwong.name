abde6eaaa52daa1aeca2d60a6dceb7965938905e
Wong Yong Jie <yjwong92@gmail.com>
2016-12-13 05:23:52 -0500

    Added projects to menu.

--- components/menu.js
+++ components/menu.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Link } from 'react-router';
 import CSSModules from 'react-css-modules';
 import classNames from 'classnames';
+
+import projects from '../public/projects.json';
 import styles from '../styles/menu.scss';
 
 class Menu extends Component {
@@ -14,6 +17,26 @@ class Menu extends Component {
     this.setState(Object.assign({}, this.state, {
       opened: !this.state.opened
     }));
+  }
+
+  onMenuItemClicked() {
+    this.setState(Object.assign({}, this.state, {
+      opened: false
+    }));
+  }
+
+  getListForProjectType(type) {
+    return (
+      <ul>
+        <For each="item" of={projects.filter(project => project.type === type)}>
+          <li key={item.id}>
+            <Link to={`/projects/${item.type}/${item.id}`} onClick={this.onMenuItemClicked.bind(this)} title={item.synopsis}>
+              {item.title}
+            </Link>
+          </li>
+        </For>
+      </ul>
+    );
   }
 
   getMenu() {
@@ -39,13 +62,22 @@ class Menu extends Component {
                 transitionLeave={false}
                 transitionAppear={true}
                 transitionAppearTimeout={500}>
-                <h2 key="about">About</h2>
-                <h2 key="code">Code</h2>
-                <h2 key="design">Design</h2>
-                <h2 key="games">Games</h2>
-                <h2 key="film">Film</h2>
-                <h2 key="others">Others</h2>
-                <h2 key="contact">Contact</h2>
+                <h3 key="about"><Link to="/about" onClick={this.onMenuItemClicked.bind(this)}>About</Link></h3>
+                <ul>
+                  <li><Link to="/about/me">Me</Link></li>
+                  <li><Link to="/about/portfolio">This Portfolio</Link></li>
+                </ul>
+                <h3 key="code"><Link to="/code" onClick={this.onMenuItemClicked.bind(this)}>Code</Link></h3>
+                {this.getListForProjectType('code')}
+                <h3 key="design"><Link to="/design" onClick={this.onMenuItemClicked.bind(this)}>Design</Link></h3>
+                {this.getListForProjectType('design')}
+                <h3 key="games"><Link to="/games" onClick={this.onMenuItemClicked.bind(this)}>Games</Link></h3>
+                {this.getListForProjectType('game')}
+                <h3 key="film"><Link to="/film" onClick={this.onMenuItemClicked.bind(this)}>Film</Link></h3>
+                {this.getListForProjectType('film')}
+                <h3 key="others"><Link to="/others" onClick={this.onMenuItemClicked.bind(this)}>Others</Link></h3>
+                {this.getListForProjectType('other')}
+                <h3 key="contact"><Link to="/contact" onClick={this.onMenuItemClicked.bind(this)}>Contact</Link></h3>
               </ReactCSSTransitionGroup>
             </div>
           </div>
